c5afdba860d031de42e396a947b9b17e29ad09f5
Monarch34 <omerzembat@hotmail.com>
2024-05-13 13:38:47 +0300

    dosya ekleme tamamlandı dosya isminde değişiklik yapılması gerekiyor

--- route/paperRoute.js
+++ route/paperRoute.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const paperController = require('../controller/paperController');
-//const upload = multer({ dest: 'files/' }); // Yüklenen dosyaların geçici olarak saklanacağı klasör
-//upload.single('pdfFile')
+const path = require('path');
 
+// Disk depolama ayarlarını belirliyoruz
 
+// Multer'ı ayarlarla birlikte kullanarak yükleme yapmak üzere hazırlıyoruz
+const upload = multer({ storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './files'); // Dosyanın kaydedileceği klasör
+    },
+    filename: function (req, file, cb) {
+      // Yüklenen dosyanın uzantısını kontrol ediyoruz
+      let extension = path.extname(file.originalname);
+      if (extension !== '.pdf') {
+        // Eğer dosya PDF değilse, uzantıyı PDF'ye çeviriyoruz
+        extension = '.pdf';
+      }
+  
+      // Yeni dosya adını oluşturuyoruz
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, 'uploaded-' + uniqueSuffix + extension); // Örneğin: uploaded-1620871190294-123456789.pdf
+    }
+  }) });
+
+// Rotaları tanımlıyoruz
 router.get('/', paperController.getPapers);
-router.post('/', paperController.createPaper);
+router.post('/', upload.single('pdfFile'), paperController.createPaper);
 router.get('/creator/:creatorId', paperController.getPapersByCreatorId);
 router.get('/conference/:conferenceId', paperController.getPapersByConferenceId);
 router.get('/:paperId', paperController.getPaperById);
